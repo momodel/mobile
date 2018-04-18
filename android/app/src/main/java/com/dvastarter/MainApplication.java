aa4a73cdfb54549c61a9aa55d3c1afb6e2beb6b9
bingweiChen <bingwei.chen11@gmail.com>
2018-04-18 16:47:33 +0800

    成功实现android语音库

--- android/app/src/main/java/com/dvastarter/MainApplication.java
+++ android/app/src/main/java/com/dvastarter/MainApplication.java
@@ -3,6 +3,7 @@ package com.dvastarter;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zphhhhh.speech.SpeechPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.wenkesj.voice.VoicePackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SpeechPackage(),
             new ImagePickerPackage(),
             new VoicePackage()
       );
